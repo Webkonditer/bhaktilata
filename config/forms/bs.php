fa13f3d9c0203ff3c8bd650accb2a29436ef0a58
The Deceased <Deceased.Ag@yandex.ru>
2017-09-02 13:37:53 +0300

    Изменения в формах

--- config/forms/bs.php
+++ config/forms/bs.php
@@ -6,13 +6,13 @@ return [
         'name' => [
             'type' => 'string',
             'caption' => 'Имя ',
-            'required' => true,
+            'required' => false,
             'placeholder' => 'Ваше имя',
         ],
         'email' => [
             'type' => 'email',
             'caption' => 'E-mail ',
-            'required' => true,
+            'required' => false,
             'placeholder' => 'Ваш e-mail',
         ],
         'phone' => [
@@ -24,20 +24,26 @@ return [
         'skype' => [
             'type' => 'string',
             'caption' => 'Skype ',
-            'required' => true,
+            'required' => false,
             'placeholder' => 'Ваш Skype, если есть ',
         ],
         'city' => [
             'type' => 'string',
             'caption' => 'Город ',
-            'required' => true,
+            'required' => false,
             'placeholder' => 'Ваш город проживания ',
         ],
         'require' => [
             'type' => 'string',
             'caption' => 'Требования к студентам ',
-            'required' => true,
+            'required' => false,
             'placeholder' => 'Подходите ли Вы под требования к студентам курса?',
+        ],
+        'time' => [
+            'type' => 'string',
+            'caption' => 'Время занятий ',
+            'required' => false,
+            'placeholder' => 'Укажите время по Мск, когда Вы точно не сможете быть на встречах с куратором ',
         ],
         'description' => [
             'type' => 'text',
