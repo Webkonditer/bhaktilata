2e43b0742d68ba7a0aac62a124b84ff0c2c12482
The Deceased <Deceased.Ag@yandex.ru>
2017-09-02 15:14:52 +0300

    Изменил текст соглашения в форме

--- resources/views/public/common/forms/form.blade.php
+++ resources/views/public/common/forms/form.blade.php
@@ -19,7 +19,7 @@
 
     @endforeach
     <div class="form-group">
-        <p class="help-block">Отправляя данную форму, даю согласие на обработку моих персональных данных в соответствии с <a class="text-info" href="https://bhaktilata.ru/docs/policy.pdf" target="_blank"> политикой в отношении обработки персональных данных</a> и <a class="text-info" href="https://bhaktilata.ru/docs/terms.pdf" target="_blank"> пользовательским соглашением.</a></p>
+        <p class="help-block"><span style="font-size: 8px">Я согласен с политикой обработки персональных данных и пользовательским соглашением.</span></p>
     </div>
     @if ($form->getParameter('show_close_modal'))
         <button type="button" class="btn btn-default" data-dismiss="modal">Отменить</button>
